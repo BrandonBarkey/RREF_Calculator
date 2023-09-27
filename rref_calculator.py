38cf4f572ef0a6ac67455a73b888d78d3945eb51
BrandonBarkey <143535932+BrandonBarkey@users.noreply.github.com>
2023-09-27 11:13:13 -0500

    Add files via upload

--- rref_calculator.py
+++ rref_calculator.py
@@ -19,6 +19,14 @@ def addRows(matrix, row1, row2, const1, const2):
 
     for num in range(0, len(matrix[row1])):
         matrix[row2][num] += matrix[row1][num]
+
+    # Reset row 1
+    count = 0
+    for number in matrix[row1]:
+        number /= const1
+        matrix[row1][count] = number
+        count += 1
+
     return row2
 
 # declarations
@@ -36,23 +44,33 @@ matrix = [[2.0, 5.0, 3.0, 7.0],
 # print(matrix)
 
 # intelligence
+# Locks off rows with leading entries
+print(matrix)
+lead_row = 0
+
 for col in range(0, len(matrix[0])):
-    # get first leading entry, make it a pivot, and set all other equal to zero
-    for row in range(0, len(matrix[0])):
+    print(f"Column: {col}")
+    # get leading entry, make it a pivot, and set all other equal to zero
+    for row in range(lead_row, len(matrix)):
         # check to see if we have a leading entry
         if matrix[row][col] != 0:
+            print(f"Leading entry in row {row}")
             multiplyRow(matrix, row, 1 / matrix[row][col])
             # bring leading entry to the top
-            if row != 0:
-                swapRow(matrix, row, 0)
+            # LEADING ENTRY WON'T ALWAYS BE IN ROW 0 NEED TO MAKE A COUNTER
+            if row != lead_row:
+                swapRow(matrix, row, lead_row)
             print(matrix)
+            lead_row += 1
             # make all other number in column zero
-            for check_row in range(0, len(matrix[0])):
-                print(check_row, row)
+            for check_row in range(0, len(matrix)):
                 if check_row != row:
-                    addRows(matrix, row, check_row, matrix[check_row][col], matrix[row][col])
-                print(matrix)
+                    if matrix[check_row][col] > 0:
+                        addRows(matrix, row, check_row, 1, -1 / matrix[row][col])
+                    else:
+                        addRows(matrix, row, check_row, 1, 1 / matrix[row][col])
+                    print(f"Set row {check_row} to zero")
+                    print(matrix)
             break
-    break
-
+print("FINAL:")
 print(matrix)
